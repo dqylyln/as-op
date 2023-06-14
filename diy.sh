8eff935d7f2bf2def2e79269ca62def50960857c
DongQiYu <dqylyln@163.com>
2023-06-14 13:35:44 +0800

    Update diy.sh
    update

--- diy.sh
+++ diy.sh
@@ -11,6 +11,9 @@ sed -i 's/192.168.1.1/192.168.1.4/g' package/base-files/files/bin/config_generat
 #### add ext luci-app
 git clone https://github.com/kenzok8/small-package package/small-package
 
+rm -rf package/small-package/firewall
+rm -rf package/small-package/firewall4
+
 ####================= customize personal info =================############
 ### replace host name
 # sed -i 's/OpenWrt/HomeNet/g' package/base-files/files/bin/config_generate
